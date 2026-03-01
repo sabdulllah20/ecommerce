c0b9c5c71ae2e8ec1ac4ef3b758155b9ee519ea6
Saad <sabdulllah20@gmail.com>
2026-03-02 01:12:52 +0500

    fix

--- src/utility/cloudinary.js
+++ src/utility/cloudinary.js
@@ -1,12 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 cloudinary.config({
-  cloud_name: "dj6ivsjr4",
-  api_key: "836384222872846",
-  api_secret: "fn4AO6vhVWH8myONihPV9BJb2jA",
-  // cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  // api_key: process.env.API_KEY,
-  // api_secret: process.env.API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
 });
 
 export const cloudinaryUpload = async function (localFilePath) {
